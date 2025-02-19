3c8b970661fd9e19c12acc432fec71daa3991e1a
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2025-02-18 19:56:56 -0500

    calc chars per min (added timer)

--- internal/test/test.go
+++ internal/test/test.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/shwaygrr/cli-typing-test/internal/ansi"
 	"golang.org/x/term"
@@ -16,35 +18,65 @@ const (
 )
 
 type Test struct {
-	expected  string
-	input     []byte
-	cursorPos int
+	expected                 string
+	input                    []byte
+	cursorPos                int
+	total_chars, total_words int
+	// wpm, cpm, accuracy float32
 }
 
 func NewTest(expected_str string) Test {
 	test := Test{
-		expected:  expected_str,
-		input:     make([]byte, len(expected_str)),
-		cursorPos: 0,
+		expected:    expected_str,
+		input:       make([]byte, len(expected_str)),
+		cursorPos:   0,
+		total_chars: len(expected_str),
+		total_words: len(strings.Trim(expected_str, " ")),
+
+		// wpm:       0,
+		// cpm:       0,
+		// accuracy:  0,
 	}
 	return test
 }
 
+func (test *Test) getExpectedChar() byte {
+	return test.expected[test.cursorPos]
+}
+
+func (test *Test) calcInputCharDiff() int {
+	diff_count := 0
+
+	for i := range test.expected {
+		if test.expected[i] != test.input[i] {
+			diff_count++
+		}
+	}
+
+	return diff_count
+}
+
 func (test *Test) handleInput(input byte) error {
-	isAllowedInput := ('A' <= input && input <= 'Z') || ('a' <= input && input <= 'z') || ('0' <= input && input <= '9') || input == ' ' || input == CTRLC || input == BACKSPACE
+	isAllowedInput := ('A' <= input && input <= 'Z') ||
+		('a' <= input && input <= 'z') ||
+		('0' <= input && input <= '9') ||
+		strings.ContainsRune("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", rune(input)) ||
+		input == ' ' ||
+		input == CTRLC ||
+		input == BACKSPACE
 
 	if !isAllowedInput {
 		return nil // not error but does nothing with input
 	}
 
 	switch input {
-	case CTRLC, ENTER: // handle end test
+	case CTRLC: // handle end test
 		return errors.New("closing test")
 
 	case BACKSPACE: // handle backspace
 		if test.cursorPos > 0 {
 			test.cursorPos--
-			ansi.BackspaceAndReplace(test.expected[test.cursorPos])
+			ansi.BackspaceAndReplace(test.getExpectedChar())
 		}
 
 	default: // handle normal input
@@ -65,10 +97,31 @@ func (test *Test) termSetup() {
 	ansi.ResetScreen()
 	ansi.ChangeTextColor(ansi.Cyan)
 	fmt.Println(test.expected)
-	ansi.WriteCharWithColor(1, 1, 0, "")
+	ansi.WriteCharWithColor(1, 1, 0, "") //move to start and
+}
+
+func (test *Test) timeCalcs(duration_minutes float32) (float32, float32) {
+	valid_chars := test.total_chars - test.calcInputCharDiff()
+	cpm := float32(valid_chars) / duration_minutes
+	wpm := float32(test.total_words) / duration_minutes
+
+	fmt.Println(ansi.Reset+"cpm:", cpm)
+	fmt.Println("\nduration:", duration_minutes)
+	fmt.Println("wpm:", wpm)
+	fmt.Println("valid_chars:", valid_chars)
+	return cpm, wpm
 }
 
 func (test *Test) RunTest() {
+	startTime := time.Now()
+	// ticker := time.NewTicker(1 * time.Second)
+	// go func() {
+	// 	for range ticker.C {
+	// 		elapsed := time.Since(startTime)
+	// 		fmt.Printf("\rTime elapsed: %.2f seconds", elapsed.Seconds())
+	// 	}
+	// }()
+
 	var file_descriptor = int(os.Stdin.Fd())
 
 	oldState, err := term.MakeRaw(file_descriptor)
@@ -81,7 +134,6 @@ func (test *Test) RunTest() {
 	defer ansi.ChangeTextColor(ansi.Reset)
 
 	for {
-		// fmt.Print(string(test.expected[test.cursorPos]))
 		b := make([]byte, 1)
 		_, err = os.Stdin.Read(b)
 		if err != nil {
@@ -90,7 +142,10 @@ func (test *Test) RunTest() {
 
 		err := test.handleInput(b[0])
 		if err != nil {
-			return
+			break
 		}
 	}
+
+	test.timeCalcs(float32(time.Since(startTime).Minutes()))
+
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/shwaygrr/cli-typing-test/internal/test"
 
 func main() {
-	test_string := "hello world"
+	test_string := "In Go, there are several ways to determine the type of a value."
 	tt := test.NewTest(test_string)
 
 	tt.RunTest()
