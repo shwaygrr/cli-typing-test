8eeac1d6f6e6d78ebdaf8ff34376047fec63bcd5
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2025-04-29 20:49:21 -0400

    fix tracker and edit metric output

--- internal/test/test.go
+++ internal/test/test.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"time"
 
+	"math"
+
 	"github.com/shwaygrr/cli-typing-test/internal/ansi"
 	"golang.org/x/term"
 )
@@ -41,7 +43,7 @@ func (cursor *cursor) writeCharWithColor(inputChar byte, colorEscapeCode string)
 }
 
 func (tracker *tracker) recordValidKey() {
-	streaksLastIndex := len(tracker.validKeysStreaks) - 1
+	streaksLastIndex := len(tracker.validKeysStreaks) - 1 //should be the same as amount of mistakes (needs refactor)
 	tracker.validKeysCount++
 	tracker.validKeysStreaks[streaksLastIndex]++
 }
@@ -93,7 +95,7 @@ func (test *Test) handleInput(input byte) error {
 		('a' <= input && input <= 'z') ||
 		('0' <= input && input <= '9') ||
 		strings.ContainsRune("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", rune(input)) ||
-		input == ' ' ||
+		// input == ' ' ||
 		input == CTRLC ||
 		input == SPACE ||
 		input == BACKSPACE
@@ -108,7 +110,6 @@ func (test *Test) handleInput(input byte) error {
 		return nil
 	}
 
-	test.tracker.totalKeyStrokes++
 	// wrong char on expected SPACE
 	if !isAllowedAtEnd {
 		if expected := test.getExpectedChar(); expected == SPACE && input != expected {
@@ -122,21 +123,27 @@ func (test *Test) handleInput(input byte) error {
 
 	switch input {
 	case CTRLC: // handle end test
+		if string(test.input[test.cursor.currWordPos:test.cursor.pos]) == test.expected[test.cursor.currWordPos:test.cursor.pos] {
+			test.tracker.validWordsCount++
+		}
 		return errors.New("closing test")
 
 	case SPACE:
 		if test.cursor.pos < len(test.expected) {
+			test.tracker.totalKeyStrokes++
 			test.input[test.cursor.pos] = byte(SPACE)
 			test.handleSpace()
 		}
 
 	case BACKSPACE: // handle backspace
 		if test.cursor.pos > test.cursor.minPos {
+			test.tracker.totalKeyStrokes++
 			test.cursor.pos--
 			ansi.BackspaceAndReplace(test.getExpectedChar())
 		}
 
 	default: // handle normal input
+		test.tracker.totalKeyStrokes++
 		test.input[test.cursor.pos] = byte(input)
 		test.cursor.pos++
 		expected := test.expected[test.cursor.pos-1]
@@ -170,7 +177,7 @@ func (test *Test) termSetup() {
 // 	return diff_count
 // }
 
-func (test *Test) computeMetrics() (accuracy, cpm, wpm, consistency float32, mistakes int) {
+func (test *Test) computeMetrics() (accuracy, cpm, wpm, consistency float32, errors int) {
 	max := func(arr []int) int {
 		max := 0
 		for _, num := range arr {
@@ -186,33 +193,37 @@ func (test *Test) computeMetrics() (accuracy, cpm, wpm, consistency float32, mis
 	wpm = float32(test.tracker.validWordsCount) / test.durationMins
 
 	consistency = float32(max(test.tracker.validKeysStreaks)) / float32(test.tracker.totalKeyStrokes)
-	mistakes = test.tracker.invalidKeysCount
+	errors = test.tracker.invalidKeysCount
 	// corrections = ...
 	return
 }
 
 func (test *Test) endTest(startTime time.Time) {
+	round := func(float float32) int {
+		return int(math.Round(float64(float)))
+	}
 	//handle tottal key strokes = 0
 	//hgandle time 0
 
 	test.durationMins = float32(time.Since(startTime).Minutes())
-	accuracy, cpm, wpm, consistency, mistakes := test.computeMetrics()
+	accuracy, cpm, wpm, consistency, errors := test.computeMetrics()
 
-	duration_type := "minutes"
+	duration_type := "m"
 	duration := test.durationMins
 
 	if duration < 1 {
 		duration *= 60
-		duration_type = "seconds"
+		duration_type = "s"
 	}
 
-	fmt.Println(ansi.Reset+"\nduration:", duration, duration_type)
-	fmt.Println("cpm:", cpm)
-	fmt.Println("wpm:", wpm)
-	fmt.Println("accuracy:", accuracy*100)
-	fmt.Println("consistency:", consistency*100)
-	fmt.Println("mistakes", mistakes)
-	fmt.Printf("%+v\n", test.tracker)
+	fmt.Printf("%s\ntime:%d%s\n", ansi.Reset, round(duration), duration_type)
+	fmt.Println("cpm:", round(cpm))
+	fmt.Println("wpm:", round(wpm))
+	fmt.Printf("accuracy: %d%%\n", round(accuracy*100))
+	fmt.Printf("cosistency: %d%%\n", round(consistency*100))
+	fmt.Println("errors:", errors)
+
+	fmt.Printf("\n%+v\n", test.tracker)
 }
 
 func (test *Test) RunTest() {
